4d4c656e67bcefcc68157b6f48d9857d11ec055f
cindy9899 <yeonhoo9899@gmail.com>
2022-08-10 15:20:32 +0900

    get whole page

--- src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/jojoldu/book/springboot/domain/posts/PostsRepository.java
@@ -1,8 +1,11 @@
 package com.jojoldu.book.springboot.domain.posts;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import java.util.List;
 public interface PostsRepository extends JpaRepository<Posts, Long>{
     //JpaRepository<Entity 클래스, PK 타입>을 상속하면
     //기본적인 CRUD 메소드 자동 생성
     //!!Entity 클래스와 기본 Entity Repository는 함께 위치해야 한다.
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
--- src/main/java/com/jojoldu/book/springboot/web/IndexController.java
+++ src/main/java/com/jojoldu/book/springboot/web/IndexController.java
@@ -1,11 +1,19 @@
 package com.jojoldu.book.springboot.web;
+import com.jojoldu.book.springboot.service.posts.PostsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
+
+    private final PostsService postsService;
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){ //서버 템플릿 안에서 사용할 수 있는 객체를 저장한다.
+        //여기서는 postsService.findAllDesc로 가져온 결과를 posts로 index.mustache에 전달한다.
+        model.addAttribute("posts", postsService.findAllDesc());
         return "index"; //앞의 경로는 src/main/resources/templates, 뒤의 확장자는 .mustache로 자동 지정
     }
 
@@ -13,4 +21,5 @@ public class IndexController {
     public String postsSave(){
         return "posts-save";
     }
+
 }
